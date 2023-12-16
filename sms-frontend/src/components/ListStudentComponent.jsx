4f9c359569b4b23af55ecbd5edad63424feb265b
Carlang77 <103078964+Carlang77@users.noreply.github.com>
2023-12-16 16:17:07 +0100

    Completed API integration with spring boot project

--- sms-frontend/src/components/ListStudentComponent.jsx
+++ sms-frontend/src/components/ListStudentComponent.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { listStudents } from '../services/StudentService'
 
 const ListStudentComponent = () => {
-  
-  const dummyData = [
-    {
-        "id": 1,
-        "firstName": "React",
-        "lastName": "Awesome",
-        "email": "awesome@react.com"
-    }
+
+   const [students, setStudents] = useState([])
 
 
-  ]
+   useEffect(() => {
+    listStudents().then((response) => {
+        setStudents(response.data);
+
+
+    }).catch(error =>{
+       console.error(error); 
+    })
+
+
+
+
+   }, [])
+
   
+//   const dummyData = [
+//     {
+//         "id": 1,
+//         "firstName": "React",
+//         "lastName": "Awesome",
+//         "email": "awesome@react.com"
+//     }
+
+
+//   ]
+  
+
+
+
     return (
     <div className='container'>
         <h2 className='text-center'> Student Web Application Final Exam Registration List</h2>
@@ -34,12 +56,12 @@ Please reserve a time slot for your project defence.</p>
             </thead>
             <tbody>
                 {
-                    dummyData.map(employee =>
-                        <tr key={employee.id}>
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.email}</td>
+                    students.map(student =>
+                        <tr key={student.id}>
+                            <td>{student.id}</td>
+                            <td>{student.firstName}</td>
+                            <td>{student.lastName}</td>
+                            <td>{student.email}</td>
                         </tr>)
 
 
